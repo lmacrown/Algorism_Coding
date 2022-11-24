90da0226bd98e246ffc1adbe00cfa9beb03f249e
lmacrown <73327230+lmacrown@users.noreply.github.com>
2022-11-24 21:00:41 +0900

    2주차 코딩

--- P1744.java
+++ P1744.java
@@ -5,41 +5,45 @@ import java.util.Scanner;
 
 public class P1744 {
 	static int K;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
 		int[] A = new int[N];
-		
+
 		for (int i = 0; i < N; i++)
 			A[i] = scan.nextInt();
 		Arrays.sort(A);
-	
+
 		int sum = 0;
-		for (int i = N - 1; i > K; i--) {
-			if (i>-1) {
+		// 양수만 존재
+		for (int i = N - 1; i > 0; i--) {
+			if (i > -1) {
+				// 근접한 두수가 양수
 				if (A[i] > 1 && A[i - 1] > 0) {
 					sum += A[i] * A[i - 1];
 					i--;
-				} 
-				else
-					sum+=A[i];
+					// 근접한 두수가 양수이나 1인 경우
+				} else if (A[i] == 1 || A[i - 1] == 1)
+					sum += A[i];
+				// 근접한 두수가 양수이거나 음수인 경우
+				else {
+					K = i;
+					break;
+				}
 			}
-			
+
 		}
-		//음수가 2개 일떄는 구현 안함
-		/*
-		 * for(int i=0;i<N;i++) { if(A[i]<=0) { K=i; }else break; }
-		 * 
-		 * int[] B= new int[K+1]; for(int i=0;i<N;i++) { if(A[i]<=0) { B[i]=A[i]; } }
-		 */
+
+		for (int j = 0; j <= K; j++) {
+			if (K - 1 >= (2 * j)) {
+				sum += A[j] * A[j + 1];
+				j++;
+			} else
+				sum += A[j];
+		}
 		
-		/*
-		 * for (int i = 0; i <= K; i++) { if (i>-1) { if (A[i] > 1 && A[i - 1] > 0) {
-		 * sum += A[i] * A[i - 1]; i--; } else sum+=A[i]; }
-		 * 
-		 * }
-		 */
 		System.out.println(sum);
 
 	}
