2bdf90146ef5674257e7cce553b6b4e789f3533a
lmacrown <73327230+lmacrown@users.noreply.github.com>
2022-12-29 22:05:41 +0900

    5주차 코딩

---
+++ P1717_AS.java
@@ -0,0 +1,61 @@
+package algorism;
+
+import java.util.Scanner;
+
+public class P1717_AS {
+	public static int[] parent;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+		parent = new int[N + 1];
+		for (int i = 0; i <= N; i++)
+			parent[i] = i;
+		for (int i = 0; i < M; i++) {
+			int question = scan.nextInt();
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			if (question == 0) {
+				union(a, b);
+				System.out.println("["+a+","+b+"] =>"+"parent["+b+"]"+"="+a);
+			}
+			else {
+				if (checkSame(a, b))
+					System.out.println("YES");
+				else
+					System.out.println("NO");
+			}
+		}
+	}
+
+	public static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a != b)
+			parent[b] = a;
+		//p[3]=1 p[6]=7 p[7]=3 p[2]=4 p[1]=1
+		//(1,3)=> p[3]=1
+		//(7,6)=> p[6]=7
+		//(3,7)=>
+	}
+
+	public static int find(int a) {
+		if (a == parent[a])
+			return a;
+		else
+			return parent[a] = find(parent[a]);
+		
+	}
+
+	public static boolean checkSame(int a, int b) {
+		a = find(a);
+		b = find(b);
+		System.out.println("checkSame : "+a+","+b);
+		if (a == b)
+			return true;
+		return false;
+	}
+
+}
